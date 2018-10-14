fa12c1161b270ca8e1f9c5183de6ffb0b7a04db6
danang <danangsukma@gmail.com>
2018-10-14 16:58:36 +0700

    Create AbstractDataAccess

---
+++ WindowsFormAsync/WindowsFormsApp1/Classes/AbstractDataAccess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1.Classes
+{
+    public abstract class AbstractDataAccess
+    {
+        public virtual Form DelegateForm { get; set; }
+        public abstract int RowCount();
+        public abstract IEnumerable<object[]> LoadData(CancellationToken ct);
+    }
+}
