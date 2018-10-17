242e701e6df4220f403825789a8b6c349f7571ad
d.sukma <d.sukma@fsoft.com.vn>
2018-10-17 19:53:18 +0700

    Make Thread-Safe Calls to Windows Forms Controls

---
+++ WindowsFormAsync/ThreadSafeCall/FormThreadSafeCall.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace ThreadSafeCall
+{
+    partial class FormThreadSafeCall
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUnsafeCall = new System.Windows.Forms.Button();
+            this.lblUnsafeCall = new System.Windows.Forms.Label();
+            this.btnSafeCall = new System.Windows.Forms.Button();
+            this.lblSafeCall = new System.Windows.Forms.Label();
+            this.btnSafeCallBackgroundWorker = new System.Windows.Forms.Button();
+            this.lblSafeCallBackgroundWorker = new System.Windows.Forms.Label();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.lblSafeCall2 = new System.Windows.Forms.Label();
+            this.lblSafeCall3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnUnsafeCall
+            // 
+            this.btnUnsafeCall.Location = new System.Drawing.Point(12, 12);
+            this.btnUnsafeCall.Name = "btnUnsafeCall";
+            this.btnUnsafeCall.Size = new System.Drawing.Size(75, 23);
+            this.btnUnsafeCall.TabIndex = 0;
+            this.btnUnsafeCall.Text = "Unsafe call";
+            this.btnUnsafeCall.UseVisualStyleBackColor = true;
+            this.btnUnsafeCall.Click += new System.EventHandler(this.btnUnsafeCall_Click);
+            // 
+            // lblUnsafeCall
+            // 
+            this.lblUnsafeCall.AutoSize = true;
+            this.lblUnsafeCall.Location = new System.Drawing.Point(130, 17);
+            this.lblUnsafeCall.Name = "lblUnsafeCall";
+            this.lblUnsafeCall.Size = new System.Drawing.Size(10, 13);
+            this.lblUnsafeCall.TabIndex = 1;
+            this.lblUnsafeCall.Text = "-";
+            // 
+            // btnSafeCall
+            // 
+            this.btnSafeCall.Location = new System.Drawing.Point(13, 52);
+            this.btnSafeCall.Name = "btnSafeCall";
+            this.btnSafeCall.Size = new System.Drawing.Size(75, 23);
+            this.btnSafeCall.TabIndex = 2;
+            this.btnSafeCall.Text = "Safe call";
+            this.btnSafeCall.UseVisualStyleBackColor = true;
+            this.btnSafeCall.Click += new System.EventHandler(this.btnSafeCall_Click);
+            // 
+            // lblSafeCall
+            // 
+            this.lblSafeCall.AutoSize = true;
+            this.lblSafeCall.Location = new System.Drawing.Point(331, 81);
+            this.lblSafeCall.Name = "lblSafeCall";
+            this.lblSafeCall.Size = new System.Drawing.Size(10, 13);
+            this.lblSafeCall.TabIndex = 3;
+            this.lblSafeCall.Text = "-";
+            // 
+            // btnSafeCallBackgroundWorker
+            // 
+            this.btnSafeCallBackgroundWorker.Location = new System.Drawing.Point(13, 102);
+            this.btnSafeCallBackgroundWorker.Name = "btnSafeCallBackgroundWorker";
+            this.btnSafeCallBackgroundWorker.Size = new System.Drawing.Size(165, 23);
+            this.btnSafeCallBackgroundWorker.TabIndex = 4;
+            this.btnSafeCallBackgroundWorker.Text = "Safe call BackgroundWorker";
+            this.btnSafeCallBackgroundWorker.UseVisualStyleBackColor = true;
+            this.btnSafeCallBackgroundWorker.Click += new System.EventHandler(this.btnSafeCallBackgroundWorker_Click);
+            // 
+            // lblSafeCallBackgroundWorker
+            // 
+            this.lblSafeCallBackgroundWorker.AutoSize = true;
+            this.lblSafeCallBackgroundWorker.Location = new System.Drawing.Point(219, 107);
+            this.lblSafeCallBackgroundWorker.Name = "lblSafeCallBackgroundWorker";
+            this.lblSafeCallBackgroundWorker.Size = new System.Drawing.Size(10, 13);
+            this.lblSafeCallBackgroundWorker.TabIndex = 5;
+            this.lblSafeCallBackgroundWorker.Text = "-";
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // lblSafeCall2
+            // 
+            this.lblSafeCall2.AutoSize = true;
+            this.lblSafeCall2.Location = new System.Drawing.Point(331, 57);
+            this.lblSafeCall2.Name = "lblSafeCall2";
+            this.lblSafeCall2.Size = new System.Drawing.Size(10, 13);
+            this.lblSafeCall2.TabIndex = 6;
+            this.lblSafeCall2.Text = "-";
+            // 
+            // lblSafeCall3
+            // 
+            this.lblSafeCall3.AutoSize = true;
+            this.lblSafeCall3.Location = new System.Drawing.Point(130, 57);
+            this.lblSafeCall3.Name = "lblSafeCall3";
+            this.lblSafeCall3.Size = new System.Drawing.Size(10, 13);
+            this.lblSafeCall3.TabIndex = 7;
+            this.lblSafeCall3.Text = "-";
+            // 
+            // FormThreadSafeCall
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 137);
+            this.Controls.Add(this.lblSafeCall3);
+            this.Controls.Add(this.lblSafeCall2);
+            this.Controls.Add(this.lblSafeCallBackgroundWorker);
+            this.Controls.Add(this.btnSafeCallBackgroundWorker);
+            this.Controls.Add(this.lblSafeCall);
+            this.Controls.Add(this.btnSafeCall);
+            this.Controls.Add(this.lblUnsafeCall);
+            this.Controls.Add(this.btnUnsafeCall);
+            this.Name = "FormThreadSafeCall";
+            this.Text = "Form Thread Safe Calls";
+            this.Load += new System.EventHandler(this.FormThreadSafeCall_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUnsafeCall;
+        private System.Windows.Forms.Label lblUnsafeCall;
+        private System.Windows.Forms.Button btnSafeCall;
+        private System.Windows.Forms.Label lblSafeCall;
+        private System.Windows.Forms.Button btnSafeCallBackgroundWorker;
+        private System.Windows.Forms.Label lblSafeCallBackgroundWorker;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.Label lblSafeCall2;
+        private System.Windows.Forms.Label lblSafeCall3;
+    }
+}
+
---
+++ WindowsFormAsync/ThreadSafeCall/FormThreadSafeCall.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreadSafeCall
+{
+    public partial class FormThreadSafeCall : Form
+    {
+        // This thread is used to demonstrate both thread-safe and
+        // unsafe ways to call a Windows Forms control.
+        private Thread demoThread = null;
+
+        // This delegate enables asynchronous calls for setting
+        // the text property on a TextBox control.
+        delegate void StringArgReturningVoidDelegate(string text);
+
+        public FormThreadSafeCall()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUnsafeCall_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.demoThread = new Thread(new ThreadStart(this.ThreadProcUnsafe));
+                this.demoThread.Start();
+            }
+            catch (Exception ex)
+            {
+                lblUnsafeCall.Text = ex.Message;
+            }
+        }
+
+        private async void btnSafeCall_Click(object sender, EventArgs e)
+        {
+            btnSafeCall.Enabled = false;
+   
+            this.demoThread = new Thread(new ThreadStart(this.ThreadProcSafe));
+            this.demoThread.Start();
+
+            var uiContext = TaskScheduler.FromCurrentSynchronizationContext();
+
+            // Start a task - this runs on the background thread...
+            Task<long> task1 = Task<long>.Factory.StartNew(() =>
+                {
+                    var watch = System.Diagnostics.Stopwatch.StartNew();
+
+                    for (int i = 1; i < 1000; i++)
+                    {
+                        Thread.Sleep(5);
+                        Task.Factory.StartNew(() =>
+                        {
+                            lblSafeCall.Text = "Delay " + i.ToString() + " has completed";
+                        }, CancellationToken.None, TaskCreationOptions.None, uiContext);
+                    }
+
+                    watch.Stop();
+                    var elapsedMs = watch.ElapsedMilliseconds;
+
+                    Task.Factory.StartNew(() =>
+                    {
+                        lblSafeCall.Text = "Elapsed: " + elapsedMs + " ms";
+                    }, CancellationToken.None, TaskCreationOptions.None, uiContext);
+
+                    return elapsedMs;
+                });
+
+            Task<long> task2 = Task<long>.Factory.StartNew(() =>
+                {
+                    var watch = System.Diagnostics.Stopwatch.StartNew();
+
+                    for (int i = 1; i < 500; i++)
+                    {
+                        Thread.Sleep(5);
+                        Task.Factory.StartNew(() =>
+                        {
+                            lblSafeCall2.Text = "Delay " + i.ToString() + " has completed";
+                        }, CancellationToken.None, TaskCreationOptions.None, uiContext);
+                    }
+
+                    watch.Stop();
+                    var elapsedMs = watch.ElapsedMilliseconds;
+
+                    Task.Factory.StartNew(() =>
+                    {
+                        lblSafeCall2.Text = "Elapsed: " + elapsedMs+ " ms";
+                    }, CancellationToken.None, TaskCreationOptions.None, uiContext);
+
+                    return elapsedMs;
+                });
+
+            await Task.WhenAll(task2, task1);
+
+            //lblSafeCall.Text = task1.Result + " ms";
+            //lblSafeCall2.Text = task2.Result + " ms";
+
+            btnSafeCall.Enabled = true;
+        }
+
+        private void ThreadProcUnsafe()
+        {
+            this.lblUnsafeCall.Text = "This text was set unsafely.";
+        }
+
+        private void ThreadProcSafe()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                Thread.Sleep(10);
+                this.SetText(String.Format("This text was set safely {0} %", i));
+            }
+        }
+
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.lblSafeCall.InvokeRequired) {
+                StringArgReturningVoidDelegate del = new StringArgReturningVoidDelegate(SetText);
+                this.Invoke(del, text);
+            }
+            else
+            {
+                lblSafeCall3.Text = text;
+            }
+        }
+
+        private void btnSafeCallBackgroundWorker_Click(object sender, EventArgs e)
+        {
+            this.backgroundWorker.RunWorkerAsync();
+        }
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.lblSafeCallBackgroundWorker.Text = "This text was set safely by BackgroundWorker.";
+        }
+
+        private void FormThreadSafeCall_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
